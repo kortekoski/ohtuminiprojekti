043b3eaea9ad4301d2fcde269fab2ae80f85739a
Tapir79 <saara.m.pakarinen@gmail.com>
2025-11-17 20:41:03 +0200

    Add seed data and migrations

---
+++ migrations/01-schema.sql
@@ -0,0 +1,9 @@
+-- references is a reserved word in SQL, so we use reference_values instead
+CREATE TABLE IF NOT EXISTS reference_values(
+  id SERIAL PRIMARY KEY,
+  --citation_key VARCHAR(100) UNIQUE DEFAULT, -- TODO: add citaion key later
+  year INT NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  type VARCHAR(50) NOT NULL
+)
