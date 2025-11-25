dba40805ecfacde32d4db2bd7941e05577ae37e9
Tapir79 <saara.m.pakarinen@gmail.com>
2025-11-25 20:32:25 +0200

    Format code

--- src/tests/services/test_validation_service.py
+++ src/tests/services/test_validation_service.py
@@ -17,7 +17,9 @@ class TestValidationService(unittest.TestCase):
 
     # ---------empty entries----------------------
     def test_empty_entries_valid(self):
-        ref = MockReference("Key1", 2020, "John Smith", "A Valid Title", RefType.ARTICLE)
+        ref = MockReference(
+            "Key1", 2020, "John Smith", "A Valid Title", RefType.ARTICLE
+        )
         self.assertTrue(ValidationService._validate_empty_entries(ref))
 
     def test_empty_entries_invalid(self):
@@ -43,16 +45,22 @@ class TestValidationService(unittest.TestCase):
     # -------------year ranges------------------------------
     def test_year_range_valid(self):
         ref = MockReference("Key1", 2020, "John Smith", "A Valid Title", "article")
-        self.assertTrue(ValidationService._validate_year_range(datetime.now().year, ref))
+        self.assertTrue(
+            ValidationService._validate_year_range(datetime.now().year, ref)
+        )
 
     def test_year_range_too_low(self):
         ref = MockReference("Key1", 999, "John Smith", "A Valid Title", "article")
-        self.assertFalse(ValidationService._validate_year_range(datetime.now().year, ref))
+        self.assertFalse(
+            ValidationService._validate_year_range(datetime.now().year, ref)
+        )
 
     def test_year_range_too_high(self):
         next_year = datetime.now().year + 1
         ref = MockReference("Key1", next_year, "John Smith", "A Valid Title", "article")
-        self.assertFalse(ValidationService._validate_year_range(datetime.now().year, ref))
+        self.assertFalse(
+            ValidationService._validate_year_range(datetime.now().year, ref)
+        )
 
     # ----------------author-------------------------------
     def test_author_valid(self):
@@ -82,16 +90,24 @@ class TestValidationService(unittest.TestCase):
         self.assertFalse(ValidationService._validate_citation_key("Invalid Key!"))
 
     def test_citation_key_starts_with_letter_valid(self):
-        self.assertTrue(ValidationService._validate_citation_key_starts_with_letter("A123"))
+        self.assertTrue(
+            ValidationService._validate_citation_key_starts_with_letter("A123")
+        )
 
     def test_citation_key_starts_with_letter_invalid(self):
-        self.assertFalse(ValidationService._validate_citation_key_starts_with_letter("1ABC"))
+        self.assertFalse(
+            ValidationService._validate_citation_key_starts_with_letter("1ABC")
+        )
 
     def test_citation_key_unique_valid(self):
-        self.assertTrue(ValidationService._validate_citation_key_unique("Key3", ["Key1", "Key2"]))
+        self.assertTrue(
+            ValidationService._validate_citation_key_unique("Key3", ["Key1", "Key2"])
+        )
 
     def test_citation_key_unique_invalid(self):
-        self.assertFalse(ValidationService._validate_citation_key_unique("Key1", ["Key1", "Key2"]))
+        self.assertFalse(
+            ValidationService._validate_citation_key_unique("Key1", ["Key1", "Key2"])
+        )
 
     # ----------------bibtex reftype---------------------------
     def test_bibtex_reftype_valid(self):
@@ -100,7 +116,6 @@ class TestValidationService(unittest.TestCase):
     def test_bibtex_reftype_invalid(self):
         self.assertFalse(ValidationService._validate_bibtex_reftype("notatype"))
 
-   
     # -------------validate_reference full IT -tests----------------------------
 
     def test_validate_reference_valid(self):
@@ -117,9 +132,7 @@ class TestValidationService(unittest.TestCase):
             ValidationService.validate_reference(ref, [])
 
     def test_validate_reference_invalid_year(self):
-        ref = MockReference(
-            "Key1", 999, "John Smith", "A Valid Title", RefType.ARTICLE
-        )
+        ref = MockReference("Key1", 999, "John Smith", "A Valid Title", RefType.ARTICLE)
         with self.assertRaises(UserInputError):
             ValidationService.validate_reference(ref, [])
 
