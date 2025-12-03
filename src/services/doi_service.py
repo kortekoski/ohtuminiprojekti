e8d10cd38618a12b19e1b4bbdf4e6fe044276cde
Jeremias Setälä <jeremias.setala@helsinki.fi>
2025-12-03 18:09:03 +0200

    write service for retrieving DOIs

--- src/services/doi_service.py
+++ src/services/doi_service.py
@@ -1,35 +1,59 @@
+"""Service for handling DOI input."""
+
 from urllib.parse import urlparse
-import requests as re
+import requests
 from util import UserInputError
 import re
 from entities.reference import Reference, RefType
 
 
-class ApiService:
+class DoiService:
+    """Container for methods for getting and handling DOIs."""
+
     def __init__(self, api_proxy: type | None = None):
         if api_proxy:
             self.api = api_proxy
         else:
             self.api = CrossrefApi()
 
+    def get_doi(self, input_string: str) -> Reference | None:
+        """
+        Get a DOI from an input string with either a URL
+        that contains a DOI or a DOI.
+        """
+        if doi := DoiService._validate_input(input_string):
+            if content := self.api.get_doi_metadata(doi):
+                return DoiService._make_reference(content)
+
     @staticmethod
     def _validate_doi(maybe_doi: str) -> bool:
+        """
+        Validate that the input string is a DOI and that it
+        has a prefix that we support.
+        """
         parts = maybe_doi.split("/")
+        if len(parts) != 2:
+            return False
+        prefix = parts[0]
+
         try:
-            int_parts = list(map(int, parts))
+            prefix_start = prefix.split(".")[0]
+            prefix_start = int(prefix_start)
+            if prefix_start != 10:
+                raise UserInputError(
+                    f"The prefix of DOI doi:{maybe_doi} is unsupported."
+                )
         except ValueError:
-            return False
-
-        if len(int_parts) != 2:
-            return False
-
-        if int_parts[0] != 10:
             return False
 
         return True
 
     @staticmethod
     def _grab_doi_from_url(maybe_valid: str) -> str | None:
+        """
+        Grab and return a DOI from an input string:
+        _grab_doi_from_url("https://example.com/doi/10.221/test") -> "10.221/test"
+        """
         url_parts = iter(maybe_valid.split("/"))
         doi = None
 
@@ -44,43 +68,49 @@ class ApiService:
 
     @staticmethod
     def _validate_input(maybe_doiable: str) -> str | None:
+        """
+        Validates input and returns the DOI contained within.
+        """
         doi = maybe_doiable
         if url_validator(maybe_doiable):
-            doi = ApiService._grab_doi_from_url(maybe_doiable)
+            doi = DoiService._grab_doi_from_url(maybe_doiable)
             if doi is None:
-                return None
+                raise UserInputError(
+                    "Input seems to be a URL that does not contain a DOI."
+                )
 
-        if ApiService._validate_doi(doi):
+        if DoiService._validate_doi(doi):
             return doi
 
-        return None
+        raise UserInputError("Input must be a DOI or a URL that contains a DOI.")
 
     @staticmethod
-    def _make_reference(content: dict[str, dict[str, dict]]) -> Reference | None:
+    def _make_reference(content: dict) -> Reference | None:
+        """
+        Make a Reference object from a crossref response python object
+        """
         status = content.get("status")
         if status != "ok":
-            return None
+            raise UserInputError("Can't find DOI.")
 
-        message_type = content.get("message_type")
+        message_type = content.get("message-type")
         if message_type != "work":
             raise UserInputError("The content the DOI points to is not for a work.")
 
         message = content.get("message")
         if message is None:
-            return None
+            return
 
-        work_type = RefType.ARTICLE
+        # work_type = RefType.ARTICLE
         content_type = message.get("type")
-        if content_type != "journal_article":
-            raise UserInputError(
-                "Only works of type or 'Article' are supported by DOI."
-            )
+        if content_type != "journal-article":
+            raise UserInputError("Only works of type or 'Article' are supported.")
 
         # The field `published` looks something like
         # 'published': {'date-parts':[[2020, 8, 17]]}
         work_date = message["published"]["date-parts"][0]
-        work_year = work_date[0]
-        work_month = work_date[1]
+        work_year = int(work_date[0])
+        work_month = int(work_date[1])
 
         work_title = message["title"]
 
@@ -98,7 +128,7 @@ class ApiService:
 
         work_issn = message["ISSN"][0]
 
-        work_publisher = message["publisher"][0]
+        work_publisher = message["publisher"]
 
         work_journal = message["container-title"][0]
 
@@ -106,9 +136,15 @@ class ApiService:
 
         work_issue = message["issue"]
 
+        work_doi = message["DOI"]
+
         work_language = message["language"]
         if work_language == "en":
             work_language = "English"
+
+        work_author = " and ".join(
+            map(lambda a: f"{a['given']} {a['family']}", message["author"])
+        )
 
         extra = {
             "month": work_month,
@@ -117,19 +153,26 @@ class ApiService:
             "journal": work_journal,
             "volume": work_volume,
             "issue": work_issue,
+            "doi": work_doi,
         }
+
+        return Reference(
+            0, "placeholder", work_year, work_author, work_title, RefType.ARTICLE, extra
+        )
 
 
 class CrossrefApi:
+    """Crossref API wrapper."""
+
     def __init__(self):
         self.base_url = "https://api.crossref.org/works"
-        self.cache: dict[str, dict] = dict()
 
     def get_doi_metadata(self, doi: str) -> dict | None:
+        """Get metadata for a DOI string."""
         url = self.base_url + "/doi/"
-        response = re.get(url + doi, allow_redirects=True)
+        response = requests.get(url + doi, allow_redirects=True)
         if response.status_code == 403:
-            return None
+            raise UserInputError("Too many requests. Try again in a moment.")
 
         if response.status_code == 404:
             raise UserInputError(f"DOI doi:{doi} does not exist.")
@@ -138,6 +181,7 @@ class CrossrefApi:
 
 
 def url_validator(url: str) -> bool:
+    """Return True iff url is a proper url."""
     try:
         parsed_url = urlparse(url)
         return all([parsed_url.scheme, parsed_url.netloc, parsed_url.path])
---
+++ src/tests/services/test_doi_service.py
@@ -0,0 +1,135 @@
+import unittest
+from unittest.mock import Mock
+from services.doi_service import DoiService, CrossrefApi
+from entities.reference import Reference
+from util import UserInputError, RefType
+
+
+class TestDoiService(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_api = Mock()
+        self.service = DoiService(self.mock_api)
+
+    def test_service_handles_correct_input(self):
+        """Service should not fail given correct input."""
+        try:
+            self.service.get_doi("10.221/test")
+        except UserInputError:
+            pass
+        self.mock_api.get_doi_metadata.assert_called_with("10.221/test")
+
+        try:
+            self.service.get_doi("https://example.com/doi/10.221/test")
+        except UserInputError:
+            pass
+        self.mock_api.get_doi_metadata.assert_called_with("10.221/test")
+
+    def test_service_fails_incorrect_input(self):
+        """Service should fail given incorrect input"""
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("")
+
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("test")
+
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("https://test.org/doi/test")
+
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("https://test.org/test")
+
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("22.222/test")
+
+    def test_service_fails_on_non_ok_message(self):
+        mock_api = Mock()
+        mock_api.get_doi_metadata.return_value = {"status": "fail"}
+        service = DoiService(mock_api)
+        with self.assertRaises(UserInputError):
+            service.get_doi("10.221/test")
+
+    def test_service_fails_on_non_work_message(self):
+        self.mock_api.get_doi_metadata.return_value = {
+            "status": "ok",
+            "message-type": "person",
+        }
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("10.221/test")
+
+    def test_service_fails_missing_message(self):
+        self.mock_api.get_goi_metadata.return_value = {
+            "status": "ok",
+            "message-type": "work",
+        }
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("10.221/test")
+
+    def test_service_fails_non_journal(self):
+        self.mock_api.get_doi_metadata.return_value = {
+            "status": "ok",
+            "message-type": "work",
+            "message": {"type": "book"},
+        }
+        with self.assertRaises(UserInputError):
+            self.service.get_doi("10.221/test")
+
+    def test_service_fails_no_message(self):
+        self.mock_api.get_doi_metadata.return_value = {
+            "status": "ok",
+            "message-type": "work",
+            "type": "journal-article",
+        }
+        self.assertIsNone(self.service.get_doi("10.221/test"))
+
+    def test_service_constructs_reference(self):
+        self.mock_api.get_doi_metadata.return_value = {
+            "status": "ok",
+            "message-type": "work",
+            "message": {
+                "type": "journal-article",
+                "published": {"date-parts": [["2020", "1", "1"]]},
+                "title": "Test title.",
+                "ISSN": ["issn00000000"],
+                "container-title": ["Journal title"],
+                "volume": "1",
+                "issue": "1",
+                "DOI": "10.221/test",
+                "language": "en",
+                "author": [
+                    {"given": "Jane", "family": "Doe"},
+                    {"given": "Jane2", "family": "Doe2"},
+                ],
+                "publisher": "AAA",
+            },
+        }
+        ref = self.service.get_doi("10.221/test")
+        self.assertIsNotNone(ref)
+        if ref is None:
+            raise Exception
+        ref.extra["month"]
+        ref.extra["issn"]
+        ref.extra["publisher"]
+        ref.extra["journal"]
+        ref.extra["volume"]
+        ref.extra["issue"]
+        ref.extra["doi"]
+
+
+class TestCrossrefApi(unittest.TestCase):
+    def setUp(self):
+        self.api = CrossrefApi()
+
+    def test_existing_doi_is_retrieved(self):
+        try:
+            self.api.get_doi_metadata("10.1136/jclinpath-2020-206745")
+        except UserInputError:
+            self.fail()
+        except:
+            self.skipTest(
+                "Can't connect to https://api.crossref.org. Check your internet connection."
+            )
+
+    def test_nonexistant_doi_raises_error(self):
+        with self.assertRaises(UserInputError):
+            self.api.get_doi_metadata("10.432/whatever")
